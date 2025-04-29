e53c722eacac6f3dde0e4986d5056be762146ba3
mehedee-hassan <razin.meha@gmail.com>
2025-04-29 23:38:58 +0900

    protobuf config

--- billing_service/build.gradle.kts
+++ billing_service/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.google.protobuf") version "0.9.4"
 }
 
 group = "com.patient.billing"
@@ -21,7 +22,32 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("javax.annotation:javax.annotation-api:1.3.2")
+	implementation("io.grpc:grpc-netty-shaded:1.68.1")
+	implementation("io.grpc:grpc-protobuf:1.68.1")
+	implementation("io.grpc:grpc-stub:1.68.1")
+	implementation("com.google.protobuf:protobuf-java:3.25.5")
 }
+
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.25.5"
+	}
+	plugins {
+		create("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java:1.68.1"
+		}
+	}
+	generateProtoTasks {
+		all().forEach { task ->
+			task.plugins {
+				create("grpc") // <--- FIXED here!
+			}
+		}
+	}
+}
+
 
 tasks.withType<Test> {
 	useJUnitPlatform()
