37dbb97c55238ca88e03c13a6ea52f37c1312937
mehedee-hassan <razin.meha@gmail.com>
2025-05-01 11:11:17 +0900

    integration test patient service

--- integration-tests/src/test/java/AuthIntegrationTest.java
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -1,4 +1,10 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 public class AuthIntegrationTest {
 
@@ -13,6 +19,58 @@ public class AuthIntegrationTest {
         // 2. Act
         // 3. Assert
 
+        String loginPayload = """
+                    {
+                        "email" : "testuser@test.com",
+                        "password" : "password123"
+                    }
+                """;
+
+        try {
+            Response response = given()
+                    .contentType("application/json")
+                    .body(loginPayload)
+                    .when()
+                    .post("/auth/login")
+                    .then()
+                    .statusCode(200)
+                    .body("token",notNullValue())
+                    .extract()
+                    .response();
+
+            System.out.println("Generated Token: "+response.jsonPath().getString("token"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
+
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidLogin(){
+        // 1. Arrange
+        // 2. Act
+        // 3. Assert
+
+        String loginPayload = """
+                    {
+                        "email" : "testuser983094@test.com",
+                        "password" : "password123"
+                    }
+                """;
+
+        try {
+            Response response = given()
+                    .contentType("application/json")
+                    .body(loginPayload)
+                    .when()
+                    .post("/auth/login")
+                    .then()
+                    .statusCode(401)
+                    .extract().response();
+
+            System.out.println("Generated Token: ");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
