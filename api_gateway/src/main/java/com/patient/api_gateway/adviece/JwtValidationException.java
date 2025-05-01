b4607f565a2b71372670e3aecd2274665b830ac9
mehedee-hassan <razin.meha@gmail.com>
2025-05-01 09:36:40 +0900

    rest unauth exception

---
+++ api_gateway/src/main/java/com/patient/api_gateway/adviece/JwtValidationException.java
@@ -0,0 +1,19 @@
+package com.patient.api_gateway.adviece;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
+public class JwtValidationException {
+    @ExceptionHandler(WebClientResponseException.Unauthorized.class)
+    public Mono<Void> handleUnauthorizedException(ServerWebExchange ex){
+        ex.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return ex.getResponse().setComplete();
+    }
+}
